caf76a916e0d28066135c80e52d32dede6e45e65
Xzud <51520484+Xzud@users.noreply.github.com>
2024-03-17 14:18:28 +0800

    Add files via upload

---
+++ MAJOR/ItemsTest/index.php
@@ -0,0 +1,22 @@
+<?php include('db_con.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="items-container">
+
+        <div class="item">
+            <img src="/ItemsTest/Items/box.jpg" width="180px" height="150px" style="object-fit: cover;" alt="picture of an item">
+        </div>
+
+        
+    </div>
+</body>
+</html>
